6b1300c476f5a2ee8d17a3951bc5ba358d9cdf8a
Ryan Simonds <ryansimonds@comcast.net>
2025-08-26 13:26:12 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+if not api_key:
+    print("API key not found.")
+else:
+    print(f'Your API key is: {api_key}')
+     # Simulated request
+    response = requests.get("https://httpbin.org/get", headers={"Authorization": f"Bearer {api_key}"})
+    print("Status code:", response.status_code)
+    print("Response:", response.json())
+    
