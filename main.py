12559598fd774388245d524bd032593bfc8296cb
Ryan Simonds <ryansimonds@comcast.net>
2025-08-27 14:52:25 -0700

    took away print statements

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ else:
     response = requests.get("https://httpbin.org/get", headers={"Authorization": f"Bearer {api_key}"})
     print("Status code:", response.status_code)
     print("Response:", response.json())
-    print("This is cool.")
-    print("wow")
+    # print("This is cool.")
+    # print("wow")
