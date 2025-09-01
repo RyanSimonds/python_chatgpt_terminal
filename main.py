42863df79219b1ff9b73d50f930f71dd65f0b8e5
Ryan Simonds <ryansimonds@comcast.net>
2025-08-31 20:16:56 -0700

    Added spacer between conversations

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ def main():
             break
 
         reply = ask_chatgpt(user_input)
-        # print("\n" + "-" * 50)
         print(f"\nAI Assistant: {reply}\n")
         print("-" * 50 + "\n")
 
